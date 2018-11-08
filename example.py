9dbddfea7f0752e04120c6a62f457f8eb86e9b24
Brett Spangler <bns5273@psu.edu>
2018-11-08 13:29:35 -0500

    removed .idea folder

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 import json
-from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud import NaturalLanguageUnderstandingV1 as NLU
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
-natural_language_understanding = NaturalLanguageUnderstandingV1(
+nlu = NLU(
 	iam_apikey='BU11gy3frJMRMKz4XQ_sPJ_HGF3p-qEr74xUlEVTWvsY',
 	version='2018-03-19'
 )
 
-response = natural_language_understanding.analyze(
+response = nlu.analyze(
 	text='IBM is an American multinational technology company '
 		 'headquartered in Armonk, New York, United States, '
 		 'with operations in over 170 countries.',
