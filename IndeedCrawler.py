587cb329c8bc1948b82831aded0b19a1579e4fda
Bearded-Baguette <dhg5054@gmail.com>
2018-11-27 17:37:54 -0500

    Changed IndeedCrawler to use BeautifulSoup library. Now grabs URLS for job posts

--- IndeedCrawler.py
+++ IndeedCrawler.py
@@ -1,18 +1,20 @@
-import scrapy
+import urllib.request
+from bs4 import BeautifulSoup
 
-class IndeedSpider(scrapy.Spider):
-	name = "indeed_spider"
-	job_title = "software engineer"
-	job_location = "Pittsburgh, PA"
-	start_urls = ['indeed.com/jobs?q='+job_title+'&l='+job_location]
-	
-	def parse(self, response):
-		JOB_CARD_SELECTOR = '.jobsearch-SerpJobCard.row.result.clickcard'
-		for jobCard in response.css(JOB_CARD_SELECTOR):
-		
-			JOB_DETAILS_SELECTOR = '.jobtitle a ::attr(href)'
-			details_page = response.css(JOB_DETAILS_SELECTOR).extract_first()
-			
-			if details_page:
-				
-	
+job_sites = []
+job_title = "software engineer"
+job_location = "Pittsburgh, PA"
+search_url = 'https://www.indeed.com/jobs?q=software+engineer&l=Pittsburgh%2C+PA'
+
+page = urllib.request.urlopen(search_url, None, None)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+for job in soup.find_all('div'):
+	if job.get('data-tn-component') != None and job.get('data-tn-component') == 'organicJob':
+		url = 'https://www.indeed.com' + job.a['href']
+		job_sites.append(url)
+
+for site in job_sites:
+	print(site)
+
