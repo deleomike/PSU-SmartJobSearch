a513a6d91c4584c50fd9d36a05671ff366dacb03
Brett Spangler <bns5273@psu.edu>
2018-12-02 14:21:00 -0500

    converted to generator

--- IndeedCrawler.py
+++ IndeedCrawler.py
@@ -7,64 +7,68 @@ from Functions import *
 from urllib.parse import urljoin
 
 
-jobs = []
-job_title = "software engineer"
-job_location = "Pittsburgh, PA"
-search_url = 'https://www.indeed.com/jobs?q=software+engineer&l=Pittsburgh%2C+PA'
-base_url = 'https://www.indeed.com'
+def posting_generator():
+	jobs = []
+	job_title = "software engineer"
+	job_location = "Pittsburgh, PA"
+	search_url = 'https://www.indeed.com/jobs?q=software+engineer&l=Pittsburgh%2C+PA'
+	base_url = 'https://www.indeed.com'
 
-next_page = urllib.request.urlopen(search_url, None, None)
+	next_page = urllib.request.urlopen(search_url, None, None)
 
-nlu = NLU(
-	iam_apikey='BU11gy3frJMRMKz4XQ_sPJ_HGF3p-qEr74xUlEVTWvsY',
-	version='2018-03-19'
-)
+	nlu = NLU(
+		iam_apikey='BU11gy3frJMRMKz4XQ_sPJ_HGF3p-qEr74xUlEVTWvsY',
+		version='2018-03-19'
+	)
+
+	def nextPage(soup):
+		print("BREAK 1")
+		next_link = soup.find("span", class_="np")
+
+		if next_link is not None:
+			print("BREAK 2")
+			next_url = next_link.find_parent("a")['href']
+			next_page = urljoin(base_url, next_url)
+			return next_page
+
+		else:
+			print("BREAK 3")
+			return 0
+
+	while True:
+		soup = BeautifulSoup(next_page, 'html.parser')
+
+		#next_page = nextPage(soup)
 
 
-def nextPage(soup):
-	print("BREAK 1")
-	next_link = soup.find("span", class_="np")
-	
-	if next_link is not None:
-		print("BREAK 2")
-		next_url = next_link.find_parent("a")['href']
-		next_page = urljoin(base_url, next_url)
-		return next_page
-	
-	else:
-		print("BREAK 3")
-		return 0
-		
+		for job in soup.find_all('div'):
+			if job.get('data-tn-component') is not None and job.get('data-tn-component') == 'organicJob':
+				url = 'https://www.indeed.com' + job.a['href']
+				response = nlu.analyze(
+					url=url,
+					features=Features(
+						entities=EntitiesOptions(
+							limit=1000
+						),
+						keywords=KeywordsOptions(
+							limit=1000
+						),
+					)
+				).get_result()
+				jobs.append(response)
+				# jsonprinter(response)
+				yield response
 
-while True:
-	soup = BeautifulSoup(next_page, 'html.parser')
-	
-	#next_page = nextPage(soup)
-	
-	
-	for job in soup.find_all('div'):
-		if job.get('data-tn-component') is not None and job.get('data-tn-component') == 'organicJob':
-			url = 'https://www.indeed.com' + job.a['href']
-			response = nlu.analyze(
-				url=url,
-				features=Features(
-					entities=EntitiesOptions(),
-					keywords=KeywordsOptions()
-				)
-			).get_result()
-			jobs.append(response)
-			jsonprinter(response)
-	
-	next_url = nextPage(soup)
-	
-	if next_url == 0:
-		break
-	else:
-		next_page = urllib.request.urlopen(next_url, None, None)
+		next_url = nextPage(soup)
+
+		if next_url == 0:
+			break
+		else:
+			next_page = urllib.request.urlopen(next_url, None, None)
+
+
+	print("END OF PROGRAM!")
 		
 		
-print("END OF PROGRAM!")
-		
-		
-	
-	
+if __name__ == '__main__':
+	posting_generator()
