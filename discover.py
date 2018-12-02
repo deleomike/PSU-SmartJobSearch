a2c41dbe05719193a7ea42c6f1efafca9161b221
TheRealNulsenator <45013457+TheRealNulsenator@users.noreply.github.com>
2018-12-02 17:03:59 -0500

    Update discover.py

--- discover.py
+++ discover.py
@@ -3,26 +3,26 @@ import os
 import json
 from watson_developer_cloud import DiscoveryV1
 
+collectionID = '9f8cf9d5-8bae-4fa4-92b0-2ff75fc4bd14'
+APIKEY = 'gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW'
+URL = "https://gateway.watsonplatform.net/discovery/api"
 #Active discovery instance
 discovery = DiscoveryV1(
     version='2017-11-07', 
-    iam_apikey='gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW', 
-    url= "https://gateway.watsonplatform.net/discovery/api"
+    iam_apikey= APIKEY, 
+    url= URL
     )
 
 
 environments = discovery.list_environments()
-print(environments)
-environment = environments.result['environments'][1]['environment_id']
+environmentID = environments.result['environments'][1]['environment_id']
 
-print(environment)
 
 #init collection
-collection = discovery.create_collection(
-        environment_id = environment, 
-       # configuration_id = environment.configuration_id, 
-        name='Collection', 
-        description='{collection_desc}').get_result()
+#collection = discovery.create_collection(
+#        environment_id = environment, 
+#        name='Collection', 
+#        description='{collection_desc}').get_result()
 
 
 data = ['1.htm','2.htm','3.htm','4.htm', '5.htm', '6.htm', '7.htm', '8.htm'] #list of names of documents
@@ -30,7 +30,7 @@ data = ['1.htm','2.htm','3.htm','4.htm', '5.htm', '6.htm', '7.htm', '8.htm'] #li
 #adds each one to the watson dictionary collection
 for file in data:
     with open((os.path.join(os.getcwd(), 'data', file))) as fileinfo: 
-                add_doc = discovery.add_document(environment, collection.collection_id, file_info=fileinfo)
+                add_doc = discovery.add_document(environmentID, collectionID, file_info=fileinfo)
                 print(json.dumps(add_doc, indent=2))
     
        
@@ -42,4 +42,3 @@ for keyword in keywords:
     #QueryPassages:
     #passage_text = actual response text
     #document_id = unique file identifier from the watson
-    
