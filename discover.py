2f46f9b840c7bb890b0e0beb43c2e2daa56377ab
TheRealNulsenator <45013457+TheRealNulsenator@users.noreply.github.com>
2018-12-02 15:51:07 -0500

    Update discover.py

--- discover.py
+++ discover.py
@@ -4,17 +4,41 @@ import json
 from watson_developer_cloud import DiscoveryV1
 
 #Active discovery instance
-discovery = DiscoveryV1(version='2017-11-07', api_key='gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW')
+discovery = DiscoveryV1(
+    version='2017-11-07', 
+    api_key='gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW', 
+    url= "https://gateway.watsonplatform.net/discovery/api")
 
-#pages refers to webpages
-#this method of uploading data into watson discover is through local files
-#html is acceptable, so we can just download the HTML file at every URL as we crawl
-#as we download, name the files by an incrementing number and append that name into the pages list
-pages = []
+environment = discovery.create_environment(
+    name="Job Files",
+    description="Holds downloaded html files found by webcrawler"
+).get_result()
 
 
-for page in pages :
-    with open((os.path.join(os.getcwd(), 'data', page))) as fileinfo: 
-        add_doc = discovery.add_document('68888984-5825-4b45-b2ba-febfacd1bf4e', 'e511d275-2a61-47dd-8bd1-79d0da5a1c61', file_info=fileinfo)
+collection = discovery.create_collection(
+        environment_id='database.environment_id', 
+        configuration_id='database_id', 
+        name='Collection', 
+        description='{collection_desc}').get_result()
+
+#this line adds 
+with open((os.path.join(os.getcwd(), '{folder name}', '{file name}'))) as fileinfo: 
+        add_doc = discovery.add_document(environment.environment_id, collection.collection_id, file_info=fileinfo)
         print(json.dumps(add_doc, indent=2))
-#this code was originally from IBM's page and it was totally broken but i fixed it because i am smarter than IBM obviously
+
+
+query(self, 
+      environment_id, 
+      collection_id, 
+      filter=None, 
+      query=None, 
+      natural_language_query=None, 
+      passages=True, aggregation=None, 
+      count=10, return_fields=None, 
+      offset=None, sort=None, highlight=None, 
+      passages_fields=None, passages_count=None, 
+      passages_characters=100, deduplicate=None, 
+      deduplicate_field=None, collection_ids=None, 
+      similar=None, similar_document_ids=None, 
+      similar_fields=None, bias=None, 
+      logging_opt_out=None, **kwargs)
