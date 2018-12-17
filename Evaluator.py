72db80188eb864c664def865a849bf1eddeee2f6
Brett Spangler <bns5273@psu.edu>
2018-12-17 13:33:11 -0500

    -> tabs

--- Evaluator.py
+++ Evaluator.py
@@ -4,56 +4,56 @@ from ResumeParser import get_letter
 
 
 class Evaluator:
-    def __init__(self, filename, limit, jobTitle, jobLocation):
-        letter = get_letter(filename)
-        
-        self.resume_words = []
-        for item in letter['keywords']:          
-            self.resume_words.append(item['text'])
-            
-        self.keywords = []
-        self.keywords_matched = []
-        urls = []
-        keywords_union = []
-        i = 0
-        for url, posting in posting_generator(jobTitle, jobLocation):
-            response = get_evaluation(posting, letter)  # in-common keywords
-            urls.append(url)
-            keywords_union.append(response)
-            self.keywords += [e['text'] for e in posting['keywords']]     # all keywords
-            self.keywords_matched += response
-            i += 1
-            if i == limit:
-                break
+	def __init__(self, filename, limit, jobTitle, jobLocation):
+		letter = get_letter(filename)
 
-		# sorts job urls and evaluations together by the length of the keyword union set :)
-        self.all_sorted = sorted(zip(urls, keywords_union), key=lambda ele: len(ele[1]), reverse=True)
-        print(self.all_sorted)
-    def getAllEvaluations(self):
-        return self.all_sorted
-    
-    def getCommonMatchedKeywords(self):
+		self.resume_words = []
+		for item in letter['keywords']:
+			self.resume_words.append(item['text'])
+
+		self.keywords = []
+		self.keywords_matched = []
+		urls = []
+		keywords_union = []
+		i = 0
+		for url, posting in posting_generator(jobTitle, jobLocation):
+			response = get_evaluation(posting, letter)  # in-common keywords
+			urls.append(url)
+			keywords_union.append(response)
+			self.keywords += [e['text'] for e in posting['keywords']]     # all keywords
+			self.keywords_matched += response
+			i += 1
+			if i == limit:
+				break
+
+			# sorts job urls and evaluations together by the length of the keyword union set :)
+		self.all_sorted = sorted(zip(urls, keywords_union), key=lambda ele: len(ele[1]), reverse=True)
+		print(self.all_sorted)
+	def getAllEvaluations(self):
+		return self.all_sorted
+
+	def getCommonMatchedKeywords(self):
 		# most commonly matched keywords already in resume
-        common_matched = {}
-        for word in self.keywords_matched:
-            if  word in common_matched:
-                common_matched[word] += 1
-            else:
-                common_matched[word] = 1
-        common_matched = sorted(common_matched, key=common_matched.get, reverse=True)
-        print('common matched keywords', common_matched)
-        return common_matched
+		common_matched = {}
+		for word in self.keywords_matched:
+			if  word in common_matched:
+				common_matched[word] += 1
+			else:
+				common_matched[word] = 1
+		common_matched = sorted(common_matched, key=common_matched.get, reverse=True)
+		print('common matched keywords', common_matched)
+		return common_matched
 
-    def getCommonAllKeywords(self):
+	def getCommonAllKeywords(self):
 		# most common keywords in job posting
-        common_all = {}
-        for word in self.keywords:
-            if word in common_all:
-                common_all[word] += 1
-            else:
-                common_all[word] = 1
-        common_all = sorted(common_all, key=common_all.get, reverse=True)
-        print('common keywords', common_all)
-        return common_all
+		common_all = {}
+		for word in self.keywords:
+			if word in common_all:
+				common_all[word] += 1
+			else:
+				common_all[word] = 1
+		common_all = sorted(common_all, key=common_all.get, reverse=True)
+		print('common keywords', common_all)
+		return common_all
 
 
--- ResumeBuilder.py
+++ ResumeBuilder.py
@@ -20,7 +20,7 @@ collectionID = None   #collection unique ID
 keywords = ['PLC', 'Linux']     #dictionary containing all keywords extracted from resume
 JobTitle = "Software Engineering"   #default job title for searching indeed
 JobLocation = "Pittsburgh"            #default job location for searching indeed
-Resume = None 
+Resume = None
 evaluator = None
 
 
@@ -28,195 +28,195 @@ evaluator = None
 #WATSON DISCOVERY ALLOWS US TO INTELLIGENTLY QUERY DOCUMENTS
 #USES NATURAL LANGUAGE PROCESSING AND TRADITIONAL SEARCH METHODS
 def InitDiscovery():
-        global discovery
-        global environmentID
-        global collectionID
-        APIKEY = 'gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW'
-        URL = "https://gateway.watsonplatform.net/discovery/api"
-        #Active discovery instance
-        discovery = DiscoveryV1(
-        version='2017-11-07', 
-        iam_apikey= APIKEY, 
-        url= URL
-        )
-        global collectionID
-        global environmentID
-        #after we initialize the discovery instance using the apikey and url, 
-        #we will need to get the environment id so that we can create a new collection
-        environments = discovery.list_environments() 
-        environmentID = environments.result['environments'][1]['environment_id']
-        #before making the new collection, makes sure that we have the room to do so
-        #by deleting the previous queries collection
-        #this is the most efficient way to ensure that the documents
-        #from previous job searches do not interfere with the queries related
-        #to the current one
-        collections = discovery.list_collections(environmentID).result['collections']
-        for collection in collections:
-            if collection['name'] == 'Collection':
-                discovery.delete_collection(environmentID,collection['collection_id'])
-        #creates the collection    
-        collection = discovery.create_collection(
-                environment_id = environmentID, 
-                name='Collection', 
-                description='{collection_desc}').get_result()
-        collectionID = collection['collection_id']
+	global discovery
+	global environmentID
+	global collectionID
+	APIKEY = 'gR4dbAo_IIcdVYAcL1VAafrQonD9FRJF-Imceur5LPXW'
+	URL = "https://gateway.watsonplatform.net/discovery/api"
+	#Active discovery instance
+	discovery = DiscoveryV1(
+		version='2017-11-07',
+		iam_apikey= APIKEY,
+		url= URL
+	)
+	global collectionID
+	global environmentID
+	#after we initialize the discovery instance using the apikey and url,
+	#we will need to get the environment id so that we can create a new collection
+	environments = discovery.list_environments()
+	environmentID = environments.result['environments'][1]['environment_id']
+	#before making the new collection, makes sure that we have the room to do so
+	#by deleting the previous queries collection
+	#this is the most efficient way to ensure that the documents
+	#from previous job searches do not interfere with the queries related
+	#to the current one
+	collections = discovery.list_collections(environmentID).result['collections']
+	for collection in collections:
+		if collection['name'] == 'Collection':
+			discovery.delete_collection(environmentID,collection['collection_id'])
+	#creates the collection
+	collection = discovery.create_collection(
+		environment_id = environmentID,
+		name='Collection',
+		description='{collection_desc}').get_result()
+	collectionID = collection['collection_id']
 
 
 
 
 def GetPostings(count):
-    global JobTitle
-    global JobLocation
-    global environmentID
-    global collectionID
-    global discovery
-    global keywords
-    global JobTitle
-    global JobLocation
-    #UPLOADS A LIST OF DOCUMENT NAMES
-    def UploadDocuments():
-        data = os.listdir('Jobs') #gets all the filenames  in the directory
-        for file in data: #iterates over each file, uploading each to watson discovery
-            with open((os.path.join(os.getcwd(), 'Jobs', file))) as fileinfo: 
-                        add_doc = discovery.add_document(environmentID, 
-                                                         collectionID, 
-                                                         file=fileinfo,   
-                                                         file_content_type = 'text/html')   
-    evaluator = Evaluator(Resume, count, JobTitle, JobLocation)
-    #updates the resume keyword list
-    keywords.clear()
-    for word in evaluator.resume_words: #makes each one accessable to the user via dropdown
-        keywords.append(word)
-    UploadDocuments() #uploads all the jobs to discovery
-    
-    
+	global JobTitle
+	global JobLocation
+	global environmentID
+	global collectionID
+	global discovery
+	global keywords
+	global JobTitle
+	global JobLocation
+	#UPLOADS A LIST OF DOCUMENT NAMES
+	def UploadDocuments():
+		data = os.listdir('Jobs') #gets all the filenames  in the directory
+		for file in data: #iterates over each file, uploading each to watson discovery
+			with open((os.path.join(os.getcwd(), 'Jobs', file))) as fileinfo:
+				add_doc = discovery.add_document(environmentID,
+				                                 collectionID,
+				                                 file=fileinfo,
+				                                 file_content_type = 'text/html')
+	evaluator = Evaluator(Resume, count, JobTitle, JobLocation)
+	#updates the resume keyword list
+	keywords.clear()
+	for word in evaluator.resume_words: #makes each one accessable to the user via dropdown
+		keywords.append(word)
+	UploadDocuments() #uploads all the jobs to discovery
+
+
 
 #QUERIES THE KEYWORD IN THE COMPILED LIST OF JOB POSTINGS USING WATSON DISCOVERY                 
 def QueryDiscovery(keyword):
-    global environmentID
-    global collectionID
-    global discovery
-    #queries keyword extracted by NLU from resume
-    response = discovery.query(environmentID,collectionID,filter=keyword,query=keyword,count=5,passages=True) 
-    results = response.result['passages'] #query returns a series of passages in which the search term appears
-    for entity in results: #prints all the passages
-        print('\n''\"...' + str(entity['passage_text']) + '...\"''\n')
+	global environmentID
+	global collectionID
+	global discovery
+	#queries keyword extracted by NLU from resume
+	response = discovery.query(environmentID,collectionID,filter=keyword,query=keyword,count=5,passages=True)
+	results = response.result['passages'] #query returns a series of passages in which the search term appears
+	for entity in results: #prints all the passages
+		print('\n''\"...' + str(entity['passage_text']) + '...\"''\n')
 
 
 
 class Redir(object):
-    # This is what we're using for the redirect, it needs a text box
-    def __init__(self, textbox):
-        self.textbox = textbox
-        self.textbox.config(state=NORMAL)
-        self.fileno = sys.stdout.fileno
+	# This is what we're using for the redirect, it needs a text box
+	def __init__(self, textbox):
+		self.textbox = textbox
+		self.textbox.config(state=NORMAL)
+		self.fileno = sys.stdout.fileno
 
 
-    def write(self, message):
-        # provides a write function for the stdout
-        self.textbox.insert(END, str(message))
+	def write(self, message):
+		# provides a write function for the stdout
+		self.textbox.insert(END, str(message))
 
 
 
 def GetResume():
-    global keywords
-    global evaluator
-    global Resume
-    # get filename, this is the bit that opens up the dialog box this will
-    # return a string of the file name you have clicked on.
-    filename = filedialog.askopenfilename()
-    if filename:
-        # Will print the file name to the text box
-        print(filename)
-        Resume = filename
-       
-        
+	global keywords
+	global evaluator
+	global Resume
+	# get filename, this is the bit that opens up the dialog box this will
+	# return a string of the file name you have clicked on.
+	filename = filedialog.askopenfilename()
+	if filename:
+		# Will print the file name to the text box
+		print(filename)
+		Resume = filename
+
+
 
 
 def InitGUI():
-    #shutil.rmtree('Jobs')
-    #os.mkdir("Jobs")
-    global keywords
-    try:
-        shutil.rmtree("Jobs") #clears out any old web search files
-    except:
-        pass
+	#shutil.rmtree('Jobs')
+	#os.mkdir("Jobs")
+	global keywords
+	try:
+		shutil.rmtree("Jobs") #clears out any old web search files
+	except:
+		pass
 
-    InitDiscovery()
-    
-    # Make the root window
-    root = Tk()
-    root.title("Resume Helper")
-    
-    #create dropdown menu
-    keyword = StringVar(root)
-    popupMenu = OptionMenu(root, keyword, *keywords)
-    Label(root, text="Choose a keyword").grid(row = 0, column = 2)
-    popupMenu.grid(row = 1, column =2)
+	InitDiscovery()
+
+	# Make the root window
+	root = Tk()
+	root.title("Resume Helper")
+
+	#create dropdown menu
+	keyword = StringVar(root)
+	popupMenu = OptionMenu(root, keyword, *keywords)
+	Label(root, text="Choose a keyword").grid(row = 0, column = 2)
+	popupMenu.grid(row = 1, column =2)
 
 
-    #creates two text entry bars to enter job location and title
-    job = StringVar()
-    jobfield = Entry(root, textvariable=job)
-    jobfield.grid(row=0, column=0)
-    job.set("{Enter Job Title}")
+	#creates two text entry bars to enter job location and title
+	job = StringVar()
+	jobfield = Entry(root, textvariable=job)
+	jobfield.grid(row=0, column=0)
+	job.set("{Enter Job Title}")
 
-    loc = StringVar()
-    locfield = Entry(root, textvariable=loc)
-    locfield.grid(row=1, column=0)
-    loc.set("{Enter Location}")
+	loc = StringVar()
+	locfield = Entry(root, textvariable=loc)
+	locfield.grid(row=1, column=0)
+	loc.set("{Enter Location}")
 
-    count = IntVar()
-    countfield = Entry(root, textvariable=count)
-    countfield.grid(row=2, column=1)
-    Label(root, text="# of search results").grid(row=1, column=1)
-    # label
+	count = IntVar()
+	countfield = Entry(root, textvariable=count)
+	countfield.grid(row=2, column=1)
+	Label(root, text="# of search results").grid(row=1, column=1)
+	# label
 
-    #defines the search function for GO
-    def go():
-            JobTitle = job.get() #first it retrieves text for job search
-            JobLocation = loc.get()
-            GetPostings(count.get()) #then it calls the actual webcrawler + watson NLU
-            menu = popupMenu["menu"]
-            menu.delete(0, "end")   #updates the dropdown menu info
-            for string in keywords:
-                menu.add_command(label=string, 
-                                 command=lambda value=string:
-                                      keyword.set(value))
-                    
-    #simple function that prints the selection and updates the display             
-    def change_dropdown(*args):
-        print( "\n" + str(keyword.get()) + ":" )
-    keyword.trace('w', change_dropdown)
-    
-    #Sets up button that initializes the search
-    getpostings = Button(root, text='GO!', command = lambda : go())
-    getpostings.grid(row=0, column=1)
-    getpostings.event_add
-    
-    getresume = Button(root, text='Get Resume File', command = lambda : GetResume())
-    getresume.grid(row=2, column=0)
-    
-    getpassages = Button(root, text='Get Passages', command = lambda : QueryDiscovery(keyword.get()))
-    getpassages.grid(row=2, column=2)
+	#defines the search function for GO
+	def go():
+		JobTitle = job.get() #first it retrieves text for job search
+		JobLocation = loc.get()
+		GetPostings(count.get()) #then it calls the actual webcrawler + watson NLU
+		menu = popupMenu["menu"]
+		menu.delete(0, "end")   #updates the dropdown menu info
+		for string in keywords:
+			menu.add_command(label=string,
+			                 command=lambda value=string:
+			                 keyword.set(value))
 
-    
-    #create scrollbar
-    scrollbar = Scrollbar(root, orient=VERTICAL)
-    scrollbar.grid(row=2, column=3, sticky=N+S+E)
-    
-    #create textbox
-    textbox = Text(root,font=("Helvetica", 10),state=DISABLED, 
-                   yscrollcommand=scrollbar.set, wrap=WORD)
-    textbox.grid(row=8, column=0, columnspan=3, sticky=N+S+W+E)
-    scrollbar.config(command=textbox.yview)
+	#simple function that prints the selection and updates the display
+	def change_dropdown(*args):
+		print( "\n" + str(keyword.get()) + ":" )
+	keyword.trace('w', change_dropdown)
 
-    #Set up the redirection from terminal to the GUI window
-    stdre = Redir(textbox)
-    sys.stdout = stdre
-    sys.stderr = stdre
-    print("Hello, please enter job title and location\n")
-    # Start the application mainloop
-    root.mainloop()
-    
+	#Sets up button that initializes the search
+	getpostings = Button(root, text='GO!', command = lambda : go())
+	getpostings.grid(row=0, column=1)
+	getpostings.event_add
+
+	getresume = Button(root, text='Get Resume File', command = lambda : GetResume())
+	getresume.grid(row=2, column=0)
+
+	getpassages = Button(root, text='Get Passages', command = lambda : QueryDiscovery(keyword.get()))
+	getpassages.grid(row=2, column=2)
+
+
+	#create scrollbar
+	scrollbar = Scrollbar(root, orient=VERTICAL)
+	scrollbar.grid(row=2, column=3, sticky=N+S+E)
+
+	#create textbox
+	textbox = Text(root,font=("Helvetica", 10),state=DISABLED,
+	               yscrollcommand=scrollbar.set, wrap=WORD)
+	textbox.grid(row=8, column=0, columnspan=3, sticky=N+S+W+E)
+	scrollbar.config(command=textbox.yview)
+
+	#Set up the redirection from terminal to the GUI window
+	stdre = Redir(textbox)
+	sys.stdout = stdre
+	sys.stderr = stdre
+	print("Hello, please enter job title and location\n")
+	# Start the application mainloop
+	root.mainloop()
+
 InitGUI()
